1103e1d1e406acb9c51880cfa3a25136d56266a3
Pedro Mihael <pedromvpaes@gmail.com>
2021-03-16 14:48:12 -0400

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Head from 'next/head'
+
+import { Container } from '../styles/pages/Home'
+
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <Head>
+        <title>Homepage</title>
+      </Head>
+    </Container>
+  )
+}
+
+export default Home
