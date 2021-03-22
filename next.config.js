41ce4e14204a7b9b0a1083976563edc2410d40fb
Pedro Mihael <pedromvpaes@gmail.com>
2021-03-22 08:50:16 -0400

    fix: importing clients components and styles

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 const withImages = require('next-images')
+const withCSS = require('@zeit/next-css')
 
 module.exports = withImages({
     esModule: true
 })
+
+module.exports = withCSS()
--- settings/clients.ts
+++ settings/clients.ts
@@ -2,19 +2,19 @@ export const clients = [
     {
         name: 'Dialog',
         subdomains: ['dialog'],
-        styles: require('../clients-settings/styles/dialog'),
-        components: require('../clients-settings/components/dialog')
+        styles: require('../_temp_clients-settings/styles/dialog'),
+        components: require('../_temp_clients-settings/components/dialog')
     },
     {
         name: 'SBT',
         subdomains: ['sbt-pwa'],
-        styles: require('../clients-settings/sbt'),
-        components: require('../clients-settings/components/sbt')
+        styles: require('../_temp_clients-settings/styles/sbt'),
+        components: require('../_temp_clients-settings/components/sbt')
     },
     {
         name: 'Klabin',
         subdomains: ['klabin-pwa', 'klapp'],
-        styles: require('../clients-settings/klabin'),
-        components: require('../clients-settings/components/klabin')
+        styles: require('../_temp_clients-settings/styles/klabin'),
+        components: require('../_temp_clients-settings/components/klabin')
     }
 ]
